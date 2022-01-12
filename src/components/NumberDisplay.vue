95980e09aafdcb4cafdb09d4ab581fc66f23d8e5
Miller Juma <miller.developer.william@gmail.com>
2022-01-12 16:12:12 +0300

    global store

--- src/components/NumberDisplay.vue
+++ src/components/NumberDisplay.vue
@@ -1,10 +1,23 @@
 <template>
-
+  <div class="container">
+    <h3>values array:{{storeState}}</h3>
+    <div>
+      <NumberSubmit/>
+    </div>
+  </div>
 </template>
 
 <script>
+import NumberSubmit from "@/components/NumberSubmit";
+import {store} from "@/assets/js/store";
 export default {
-  name: "NumberDisplay"
+  name: "NumberDisplay",
+  components: {NumberSubmit},
+  data(){
+    return{
+      storeState: store.state
+    }
+  }
 }
 </script>
 
--- src/components/NumberSubmit.vue
+++ src/components/NumberSubmit.vue
@@ -1,10 +1,25 @@
 <template>
-$END$
+  <div class="container">
+    <input type="text" v-model="number" />
+    <button @click="addNumber(number)">Add New Number</button>
+  </div>
 </template>
 
 <script>
+import {store} from "@/assets/js/store";
+
 export default {
-name: "NumberSubmit"
+  name: "NumberSubmit",
+  data(){
+    return {
+      number:0
+    }
+  },
+  methods:{
+    addNumber(number){
+      store.addNewNumber(Number(number));
+    }
+  }
 }
 </script>
 
